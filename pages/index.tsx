d146209a273c4ebf6c0c5c210bc5a0c260b10f3e
Marco Bertonati <marko.bertonati@gmail.com>
2022-10-04 13:51:32 -0300

    Using Figma tokens as css file

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,9 @@ const Home: NextPage = () => {
           My Zoleo App
         </h1>
 
-        <p >
+        <h2 >
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates cum, reprehenderit sunt quisquam cumque enim autem magni quam saepe ut placeat. Aperiam aliquid voluptas iure! Laboriosam ducimus omnis eveniet. Quo!
-        </p>
+        </h2>
       </main>
 
       </PageLayout>
